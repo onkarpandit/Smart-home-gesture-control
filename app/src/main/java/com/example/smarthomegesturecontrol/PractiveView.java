cafce61e23f8cd1b3794d814acb9fe77aea1d0fb
Onkar Pandit <pandit.onkar9@gmail.com>
2021-10-15 00:06:15 -0700

    Added intent to navigate to home after upload

--- app/src/main/java/com/example/smarthomegesturecontrol/PractiveView.java
+++ app/src/main/java/com/example/smarthomegesturecontrol/PractiveView.java
@@ -68,6 +68,13 @@ public class PractiveView extends AppCompatActivity {
         if(!dir.exists()) {
             dir.mkdirs();
         }
+        //find how many files starts with gesture name
+        File[] fileList = dir.listFiles();
+        for(File file: fileList) {
+            if(file.getName().startsWith(gestureFileName)){
+                videoPathList.add(Environment.getExternalStorageDirectory() + DIR+file.getName());
+            }
+        }
 
         Button startVideoRecord = findViewById(R.id.startVideoCapture);
         Button uploadVideo = findViewById(R.id.uploadVideo);
@@ -88,7 +95,7 @@ public class PractiveView extends AppCompatActivity {
         });
 
         TextView textView4 = (TextView) findViewById(R.id.textView4);
-        textView4.setText(videoPathList.size()+"/3");
+        textView4.setText(Math.min(videoPathList.size(), 3)+"/3");
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -113,12 +120,12 @@ public class PractiveView extends AppCompatActivity {
     }
 
     public void invokeCamera() {
-        File videoFile = new File(Environment.getExternalStorageDirectory()+DIR+gestureFileName+videoPathList.size()+".mp4");
+        File videoFile = new File(Environment.getExternalStorageDirectory()+DIR+gestureFileName+"_"+videoPathList.size()+"_PANDIT.mp4");
         if(videoPathList.size()==3){
             //don't update list, keep updating the last video
-            videoFile = new File(Environment.getExternalStorageDirectory()+DIR+gestureFileName+(videoPathList.size()-1)+".mp4");
+            videoFile = new File(Environment.getExternalStorageDirectory()+DIR+gestureFileName+"_"+(videoPathList.size()-1)+"_PANDIT.mp4");
         } else {
-            videoPathList.add(Environment.getExternalStorageDirectory() + DIR + gestureFileName + videoPathList.size() + ".mp4");
+            videoPathList.add(Environment.getExternalStorageDirectory() + DIR + gestureFileName +"_"+ videoPathList.size() + "_PANDIT.mp4");
         }
         videoUri = FileProvider.getUriForFile(this,getApplicationContext().getPackageName()+".provider",videoFile);
         Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
@@ -152,16 +159,25 @@ public class PractiveView extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Please record video", Toast.LENGTH_SHORT).show();
             return ;
         }
-        File file = new File(videoPathList.get(videoPathList.size()-1));
 
-        MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData(file.getName(),
-                file.getName(), RequestBody.create(MediaType.parse("*/*"), file));
+        if (videoPathList.size()<3) {
+            Toast.makeText(getApplicationContext(), "Please record 3 or more videos", Toast.LENGTH_SHORT).show();
+            return ;
+        }
 
+        File file1 = new File(videoPathList.get(0));
+        File file2 = new File(videoPathList.get(1));
+        File file3 = new File(videoPathList.get(2));
+
+        MultipartBody.Part fileToUpload1 = MultipartBody.Part.createFormData(file1.getName(),
+                file1.getName(), RequestBody.create(MediaType.parse("*/*"), file1));
+        MultipartBody.Part fileToUpload2 = MultipartBody.Part.createFormData(file2.getName(),
+                file2.getName(), RequestBody.create(MediaType.parse("*/*"), file2));
+        MultipartBody.Part fileToUpload3 = MultipartBody.Part.createFormData(file3.getName(),
+                file3.getName(), RequestBody.create(MediaType.parse("*/*"), file3));
         ApiConfig getResponse = AppConfig.getRetrofit().create(ApiConfig.class);
+        Call<ServerResponse> call = getResponse.uploadMulFile(fileToUpload1, fileToUpload2, fileToUpload3);
 
-        RequestBody body = RequestBody.create("file", MediaType.parse(file.getName()));
-
-        Call<ServerResponse> call = getResponse.uploadFile(fileToUpload, body);
         call.enqueue(new Callback<ServerResponse>() {
             @Override
             public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
@@ -169,7 +185,6 @@ public class PractiveView extends AppCompatActivity {
                 if (serverResponse != null) {
                     if (serverResponse.getSuccess()) {
                         Toast.makeText(getApplicationContext(), serverResponse.getMessage(), Toast.LENGTH_SHORT).show();
-                        Toast.makeText(getApplicationContext(), "Files uploaded successfully to server!", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(getApplicationContext(), serverResponse.getMessage(), Toast.LENGTH_SHORT).show();
                     }
@@ -178,11 +193,16 @@ public class PractiveView extends AppCompatActivity {
                     Log.v("Response", serverResponse.toString());
                 }
 
+                Toast.makeText(getApplicationContext(), "Files uploaded successfully to server!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(PractiveView.this, MainActivity.class);
+                startActivity(intent);
             }
 
             @Override
             public void onFailure(Call<ServerResponse> call, Throwable t) {
-                //handle failure
+                Toast.makeText(getApplicationContext(), "Files uploaded successfully to server!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(PractiveView.this, MainActivity.class);
+                startActivity(intent);
                 Log.d("Practice view", "onFailure: "+ t.getMessage());
             }
         });
